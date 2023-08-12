273c084f5c49219505d5a087f538e81fccb3e79f
lex23unwin <lex23unwin@gmail.com>
2023-08-12 09:36:48 -0500

    Finished JS functionality of calc

--- script.js
+++ script.js
@@ -1,60 +1,146 @@
 let clearButton = document.querySelector(".calculator-clear")
 let deleteButton = document.querySelector(".calculator-delete")
-let calculatorScreen = document.querySelector(".calculator-screen")
-
-clearButton.addEventListener("click", () => {
-    calculatorScreen.textContent = ""
-})
-
-deleteButton.addEventListener("click", () => {
-    calculatorScreen.textContent = 
-        calculatorScreen.textContent.slice(0, -1);
-})
+let calculatorScreenContent = document.querySelector(".calculator-screen-content")
+let numberButtons = document.querySelectorAll(".number")
+let operatorButtons = document.querySelectorAll(".operator")
+let equalsButton = document.querySelector(".equals")
+let decimalButton = document.querySelector(".decimal")
 
 let operand1;
 let operator;
 let operand2;
 
-function add(x,y)
-{
-    return x + y;
-}
-
-function subtract(x,y)
-{
-    return x - y;
-}
-
-function multiply(x,y)
-{
-    return x * y;
-}
-
-function divide(x,y)
-{
-    return x / y;
-}
-
-function operate(operand1, operator, operand2)
-{
-    if (operator === "+")
+decimalButton.addEventListener("click", () => {
+    if (!((operand1.includes(".")) && operand2.includes(".")))
     {
-        add(operand1,operand2)
+      if (operator === "")
+      {
+          if (!(operand1.includes(".")))
+          {
+            operand1 += "."
+            calculatorScreenContent.textContent += "."; 
+          }
+      }
+      else if (operator !== "")
+      {
+          if (!(operand2.includes(".")))
+          {
+            operand2 += "."
+            calculatorScreenContent.textContent += "."; 
+          }
+      }
     }
-    else if (operator === "-")
-    {
-        subtract(operand1, operand2)
-    }
-    else if (operator === "*")
-    {
-        multiply(operand1, operand2)
-    }
-    else if (operator === "/")
-    {
-        divide(operand1, operand2)
-    }
+})
+
+equalsButton.addEventListener("click", () => {
+      if ((operand1 !== "") && (operator !== "") && (operand2 !== ""))
+      {
+        let results = operate(operand1, operator, operand2);
+        let newResults = results.toString();
+        newResults = truncateDecimal(newResults);
+        calculatorScreenContent.textContent = newResults;
+        operand1 = newResults;
+        operator = "";
+        operand2 = "";
+      }
+})
+
+function truncateDecimal(result)
+{
+  return result.slice(0, 10);
 }
 
+numberButtons.forEach( (eachNumber) => {
+  eachNumber.addEventListener("click", () => {
+    if (!(isOverflow()))
+    {
+      if (operator === "")
+      {
+        operand1 += eachNumber.textContent;
+        calculatorScreenContent.textContent += eachNumber.textContent;
+      }
+      else if (operator !== "")
+      {
+        operand2 += eachNumber.textContent;
+        calculatorScreenContent.textContent += eachNumber.textContent;
+      }
+    }
+  })
+})
 
+operatorButtons.forEach( (operatorButton) => {
+  operatorButton.addEventListener("click", () => {
+    if (operator === "")
+    {
+       operator = operatorButton.textContent;
+       calculatorScreenContent.textContent += operatorButton.textContent;
+    }
+    
+  })
+})
 
+clearButton.addEventListener("click", () => {
+    calculatorScreenContent.textContent = "";
+    clearAll();
+})
 
+deleteButton.addEventListener("click", () => {
+    calculatorScreenContent.textContent = 
+        calculatorScreenContent.textContent.slice(0, -1);
+})
+
+function clearAll()
+{
+    operand1 = "";
+    operator = "";
+    operand2 = "";
+}
+
+function isOverflow()
+{
+  if (calculatorScreenContent.textContent.length > 12)
+  {
+    return true;
+  }
+  return false;
+}
+
+function add(op1, op2)
+{
+  return op1 + op2;
+}
+
+function subtract(op1, op2)
+{
+  return op1 - op2;
+}
+
+function multiply(op1, op2)
+{
+  return op1 * op2;
+}
+
+function divide(op1, op2)
+{
+  return op1 / op2;
+}
+
+function operate(op1, operator, op2)
+{
+  if (operator === "+")
+  {
+     return add(op1, op2);
+  }
+  else if (operator === "-")
+  {
+    return subtract(op1, op2);
+  }
+  else if (operator === "*")
+  {
+    return multiply(op1, op2);
+  }
+  else 
+  {
+    return divide(op1, op2);
+  }
+}
