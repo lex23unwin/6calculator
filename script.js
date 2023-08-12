58bc85edc1845f54c6dfa74782451a16af98b8f7
lex23unwin <lex23unwin@gmail.com>
2023-08-12 09:35:08 -0500

    Updated html for more JS featrues

--- script.js
+++ script.js
@@ -11,10 +11,49 @@ deleteButton.addEventListener("click", () => {
         calculatorScreen.textContent.slice(0, -1);
 })
 
+let operand1;
+let operator;
+let operand2;
 
+function add(x,y)
+{
+    return x + y;
+}
 
+function subtract(x,y)
+{
+    return x - y;
+}
 
+function multiply(x,y)
+{
+    return x * y;
+}
 
+function divide(x,y)
+{
+    return x / y;
+}
+
+function operate(operand1, operator, operand2)
+{
+    if (operator === "+")
+    {
+        add(operand1,operand2)
+    }
+    else if (operator === "-")
+    {
+        subtract(operand1, operand2)
+    }
+    else if (operator === "*")
+    {
+        multiply(operand1, operand2)
+    }
+    else if (operator === "/")
+    {
+        divide(operand1, operand2)
+    }
+}
 
 
 
