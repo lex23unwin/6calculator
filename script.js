8d62f5fc91766c5d85d7a888ea666926806c224f
lex23unwin <lex23unwin@gmail.com>
2023-09-17 19:08:12 -0500

    addition operation fixed

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let clearButton = document.querySelector(".calculator-clear")
-let deleteButton = document.querySelector(".calculator-delete")
-let calculatorScreenContent = document.querySelector(".calculator-screen-content")
-let numberButtons = document.querySelectorAll(".number")
-let operatorButtons = document.querySelectorAll(".operator")
-let equalsButton = document.querySelector(".equals")
-let decimalButton = document.querySelector(".decimal")
+let clearButton = document.querySelector(".calculator-clear");
+let deleteButton = document.querySelector(".calculator-delete");
+let calculatorScreenContent = document.querySelector(".calculator-screen-content");
+let numberButtons = document.querySelectorAll(".number");
+let operatorButtons = document.querySelectorAll(".operator");
+let equalsButton = document.querySelector(".equals");
+let decimalButton = document.querySelector(".decimal");
 
-let operand1;
-let operator;
-let operand2;
+let operand1 = "";
+let operator = "";
+let operand2 = "";
 
 decimalButton.addEventListener("click", () => {
   if (operator === "") 
@@ -30,96 +30,97 @@ decimalButton.addEventListener("click", () => {
 });
 
 equalsButton.addEventListener("click", () => {
-      if ((operand1 !== "") && (operator !== "") && (operand2 !== ""))
-      {
-        let results = operate(operand1, operator, operand2);
-        let newResults = results.toString();
-        newResults = truncateDecimal(newResults);
-        calculatorScreenContent.textContent = newResults;
-        operand1 = newResults;
-        operator = "";
-        operand2 = "";
-      }
-})
+  if (operand1 !== "" && operator !== "" && operand2 !== "") 
+  {
+    let result = operate(operand1, operator, operand2);
+    let truncatedResult = truncateDecimal(result.toString());
+    calculatorScreenContent.textContent = truncatedResult;
+    operand1 = truncatedResult;
+    operator = "";
+    operand2 = "";
+  }
+});
 
-function truncateDecimal(result)
-{
+function truncateDecimal(result) {
   return result.slice(0, 10);
 }
 
-numberButtons.forEach( (eachNumber) => {
+numberButtons.forEach((eachNumber) => {
   eachNumber.addEventListener("click", () => {
-    if (!(isOverflow()))
-    {
-      if (operator === "")
-      {
+    if (!isOverflow()) {
+      if (operator === "") {
         operand1 += eachNumber.textContent;
         calculatorScreenContent.textContent += eachNumber.textContent;
-      }
-      else if (operator !== "")
+      } 
+      else if (operator !== "") 
       {
         operand2 += eachNumber.textContent;
         calculatorScreenContent.textContent += eachNumber.textContent;
       }
     }
-  })
-})
+  });
+});
 
-operatorButtons.forEach( (operatorButton) => {
+operatorButtons.forEach((operatorButton) => {
   operatorButton.addEventListener("click", () => {
-    if (operator === "")
+    if (operator === "") 
     {
-       operator = operatorButton.textContent;
-       calculatorScreenContent.textContent += operatorButton.textContent;
+      operator = operatorButton.textContent;
+      calculatorScreenContent.textContent += operatorButton.textContent;
+    } 
+    else 
+    {
+      // If an operator is already set, calculate the result
+      if (operand1 !== "" && operand2 !== "") 
+      {
+        let result = operate(operand1, operator, operand2);
+        let truncatedResult = truncateDecimal(result.toString());
+        calculatorScreenContent.textContent = truncatedResult;
+        operand1 = truncatedResult;
+        operator = operatorButton.textContent;
+        operand2 = "";
+      }
     }
-    
-  })
-})
+  });
+});
 
 clearButton.addEventListener("click", () => {
-    calculatorScreenContent.textContent = "";
-    clearAll();
-})
+  calculatorScreenContent.textContent = "";
+  clearAll();
+});
 
 deleteButton.addEventListener("click", () => {
-    calculatorScreenContent.textContent = 
-        calculatorScreenContent.textContent.slice(0, -1);
-})
+  calculatorScreenContent.textContent = calculatorScreenContent.textContent.slice(0, -1);
+});
 
-function clearAll()
-{
-    operand1 = "";
-    operator = "";
-    operand2 = "";
+function clearAll() {
+  operand1 = "";
+  operator = "";
+  operand2 = "";
 }
 
-function isOverflow()
-{
-  if (calculatorScreenContent.textContent.length > 12)
-  {
-    return true;
-  }
-  return false;
+function isOverflow() {
+  return calculatorScreenContent.textContent.length > 12;
 }
 
-function add(op1, op2)
+function add(op1, op2) 
 {
-  return op1 + op2;
+  return parseFloat(op1) + parseFloat(op2);
 }
 
-function subtract(op1, op2)
+function subtract(op1, op2) 
 {
-  return op1 - op2;
+  return parseFloat(op1) - parseFloat(op2);
 }
 
-function multiply(op1, op2)
+function multiply(op1, op2) 
 {
-  return op1 * op2;
+  return parseFloat(op1) * parseFloat(op2);
 }
 
-function divide(op1, op2)
+function divide(op1, op2) 
 {
-  return op1 / op2;
+  return parseFloat(op1) / parseFloat(op2);
 }
 
 function operate(op1, operator, op2) 
@@ -136,12 +137,11 @@ function operate(op1, operator, op2)
   {
     return multiply(op1, op2);
   } 
-  else if (operator === "/" && op2 !== 0) 
+  else if (operator === "/" && op2 !== "0") 
   {
     return divide(op1, op2);
   } 
-  else 
-  {
+  else {
     return "Error";
   }
 }
